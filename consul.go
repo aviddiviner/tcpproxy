c683ba7e7b7a892f7a46f60e9d42c70de838dd2c
David Irvine <aviddiviner@gmail.com>
2018-05-07 17:18:28 -0500

    Add ConsulMatcher, with DNS lookups and tests

---
+++ consul.go
@@ -0,0 +1,145 @@
+package tcpproxy
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+// dns.Client interface used to make testing easier; we use a mock client for the tests
+type dnsClient interface {
+	ExchangeContext(ctx context.Context, m *dns.Msg, a string) (r *dns.Msg, rtt time.Duration, err error)
+}
+
+// ConsulMatcher returns a Matcher that directs any hostname ending with its matchSuffix
+// to the service name registered in Consul corresponding to that host's subdomain.
+//
+// For example, if the chosen suffix is "localhost" then "foo.localhost" will be directed
+// to the consul service named "foo".
+// Note "foo.bar.localhost" will _not_ match the suffix "localhost", only "bar.localhost".
+//
+// A DNS lookup to consulDnsAddr is done to resolve the service ip:port, analogous to:
+//
+//  dig @127.0.0.1 -p 8600 _foo._tcp.consul SRV
+//
+func ConsulMatcher(matchSuffix, consulDnsAddr string) Matcher {
+	m := consulMatcher{
+		matchSuffix:   "." + strings.TrimLeft(matchSuffix, "."),
+		consulDnsAddr: consulDnsAddr,
+
+		udpClient: &dns.Client{Net: "udp"},
+		tcpClient: &dns.Client{Net: "tcp"},
+		//targets:   make(map[string]*DialProxy),
+	}
+	return m.Lookup
+}
+
+type consulMatcher struct {
+	matchSuffix   string
+	consulDnsAddr string
+
+	udpClient dnsClient
+	tcpClient dnsClient
+	//targets   map[string]*DialProxy
+}
+
+func (m *consulMatcher) Lookup(ctx context.Context, hostname string) (ok bool, target Target) {
+	ok, serviceName := m.match(ctx, hostname)
+	if !ok {
+		return
+	}
+	addr, err := m.lookupDns(ctx, serviceName)
+	if err != nil {
+		return false, nil // TODO: Do something smarter on error
+	}
+	target = To(addr) // TODO: Reuse targets
+	return
+}
+
+func (m *consulMatcher) match(_ context.Context, hostname string) (ok bool, service string) {
+	if strings.HasSuffix(hostname, m.matchSuffix) {
+		prefix := hostname[:len(hostname)-len(m.matchSuffix)]
+		if prefix == "" || strings.Contains(prefix, ".") {
+			return false, ""
+		}
+		return true, prefix
+	}
+	return false, ""
+}
+
+func (m *consulMatcher) dnsSrvQuery(ctx context.Context, fqdn string) (ans, extra []dns.RR, err error) {
+	msg := new(dns.Msg)
+	msg.SetQuestion(fqdn, dns.TypeSRV)
+
+	var r *dns.Msg
+	r, _, err = m.udpClient.ExchangeContext(ctx, msg, m.consulDnsAddr)
+	if err != nil {
+		return
+	}
+	if r.Truncated {
+		r, _, err = m.tcpClient.ExchangeContext(ctx, msg, m.consulDnsAddr)
+		if err != nil {
+			return
+		}
+	}
+
+	switch r.Rcode {
+	case dns.RcodeSuccess:
+		return r.Answer, r.Extra, nil // success
+
+	case dns.RcodeNameError:
+		err = fmt.Errorf("non-existent domain: %s\n%s", fqdn, r)
+		return
+	}
+	err = fmt.Errorf("lookup failed for: %s\n%s", fqdn, r)
+	return
+}
+
+func (m *consulMatcher) lookupDns(ctx context.Context, serviceName string) (addr string, err error) {
+	fqdn := "_" + serviceName + "._tcp.consul."
+	ans, extra, err := m.dnsSrvQuery(ctx, fqdn)
+	if err != nil {
+		return
+	}
+	var (
+		port uint16
+		node string
+		host net.IP
+	)
+
+	ok := false
+	for _, rr := range ans { // "answer" section; SRV records
+		var srv *dns.SRV
+		srv, ok = rr.(*dns.SRV)
+		if ok {
+			port, node = srv.Port, srv.Target
+			break
+		}
+	}
+	if !ok {
+		err = fmt.Errorf("no valid SRV record found for: %s\n%s", fqdn, ans)
+		return
+	}
+
+	ok = false
+	for _, rr := range extra { // "additional" section; A and TXT records
+		var a *dns.A
+		a, ok = rr.(*dns.A)
+		if ok {
+			if a.Hdr.Name == node {
+				host = a.A
+				break
+			}
+		}
+	}
+	if !ok {
+		err = fmt.Errorf("no valid A record found for: %s\n%s", fqdn, ans)
+		return
+	}
+
+	return fmt.Sprintf("%s:%d", host, port), nil
+}
---
+++ consul_test.go
@@ -0,0 +1,97 @@
+package tcpproxy
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/miekg/dns"
+)
+
+type mockDnsClient struct {
+	Answers, Extras []string
+	Truncate        bool
+	Error           error
+}
+
+func (c mockDnsClient) ExchangeContext(_ context.Context, req *dns.Msg, _ string) (r *dns.Msg, _ time.Duration, err error) {
+	if c.Error != nil {
+		err = c.Error
+		return
+	}
+	r = new(dns.Msg)
+	r.SetReply(req)
+	r.Truncated = c.Truncate
+	for _, a := range c.Answers {
+		rr, _ := dns.NewRR(a)
+		r.Answer = append(r.Answer, rr)
+	}
+	for _, e := range c.Extras {
+		rr, _ := dns.NewRR(e)
+		r.Extra = append(r.Extra, rr)
+	}
+	return
+}
+
+func TestConsulMatcher(t *testing.T) {
+	tests := []struct {
+		name   string
+		body   io.Reader
+		suffix string
+		match  bool
+
+		answer, extra []string
+		target        string
+	}{
+		{
+			name:   "match",
+			body:   strings.NewReader("GET / HTTP/1.1\r\nHost: foo.localhost\r\n\r\n"),
+			suffix: ".localhost",
+			match:  true,
+
+			answer: []string{
+				`_foo._tcp.consul.	0	IN	SRV	1 1 51885 MacBook-Pro.local.node.dc1.consul.`,
+			},
+			extra: []string{
+				`MacBook-Pro.local.node.dc1.consul. 0 IN A	127.0.0.1`,
+				`MacBook-Pro.local.node.dc1.consul. 0 IN TXT "consul-network-segment="`,
+			},
+			target: "127.0.0.1:51885",
+		},
+		{
+			name:   "no-match",
+			body:   strings.NewReader("GET / HTTP/1.1\r\nHost: foo.bar.localhost\r\n\r\n"),
+			suffix: ".localhost",
+			match:  false,
+		},
+	}
+	for i, tt := range tests {
+		name := tt.name
+		if name == "" {
+			name = fmt.Sprintf("test_index_%d", i)
+		}
+		t.Run(name, func(t *testing.T) {
+			br := bufio.NewReader(tt.body)
+			m := consulMatcher{
+				matchSuffix: tt.suffix,
+				udpClient:   mockDnsClient{Answers: tt.answer, Extras: tt.extra},
+			}
+			r := httpHostMatch{m.Lookup}
+			target := r.match(br)
+			got := target != nil
+			if got != tt.match {
+				t.Fatalf("match = %v; valid %v", got, tt.match)
+			}
+			if tt.match {
+				proxy := target.(*DialProxy)
+				if proxy.Addr != tt.target {
+					t.Fatalf("target = %v; valid %v", proxy.Addr, tt.target)
+				}
+			}
+		})
+	}
+}
